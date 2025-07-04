b54adaf0298356efc732c2521768d9976121fed6
Kev-Hernandez <133778379+Kev-Hernandez@users.noreply.github.com>
2025-07-04 12:43:40 -0600

    Agregar BrowserRouter con basename para deploy en GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Register from './components/Register';
@@ -6,12 +6,14 @@ import Chat from './components/chat/Chat';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-      <Route path="/chat" element={<Chat />} />
-    </Routes>
+    <BrowserRouter basename="/takeabreak_kev">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
